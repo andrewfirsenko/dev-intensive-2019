8e52963d12435608663950fcd8cb80d6282c60d5
Andrew <andrewfirsenko@gmail.com>
2020-08-12 16:47:19 +0300

    Done home task 4

--- app/src/main/java/ru/skillbranch/devintensive/viewmodels/ProfileViewModel.kt
+++ app/src/main/java/ru/skillbranch/devintensive/viewmodels/ProfileViewModel.kt
@@ -1,6 +1,7 @@
-package ru.skillbranch.devintensive.viewmodel
+package ru.skillbranch.devintensive.viewmodels
 
 import android.util.Log
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -11,10 +12,21 @@ class ProfileViewModel : ViewModel() {
 
     private val repository : PreferencesRepository = PreferencesRepository
     private val profileData = MutableLiveData<Profile>()
+    private val appTheme = MutableLiveData<Int>()
 
     init {
         Log.d("M_ProfileViewModel", "init view model")
         profileData.value = repository.getProfile()
+        appTheme.value = repository.getAppTheme()
+    }
+
+    fun getProfileData(): LiveData<Profile> = profileData
+
+    fun getAppTheme(): LiveData<Int> = appTheme
+
+    fun saveProfileData(profile: Profile) {
+        repository.saveProfile(profile)
+        profileData.value = profile
     }
 
     override fun onCleared() {
@@ -22,10 +34,12 @@ class ProfileViewModel : ViewModel() {
         Log.d("M_ProfileViewModel", "view model cleared")
     }
 
-    fun getProfileData(): LiveData<Profile> = profileData
-
-    fun saveProfileData(profile: Profile) {
-        repository.saveProfile(profile)
-        profileData.value = profile
+    fun switchTheme() {
+        if (appTheme.value == AppCompatDelegate.MODE_NIGHT_YES) {
+            appTheme.value = AppCompatDelegate.MODE_NIGHT_NO
+        } else {
+            appTheme.value = AppCompatDelegate.MODE_NIGHT_YES
+        }
+        repository.saveAppTheme(appTheme.value!!)
     }
 }
